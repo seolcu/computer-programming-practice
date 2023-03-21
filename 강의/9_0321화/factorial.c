de8646cf894f906767f067f36e728b0f725de350
seolcu <seolcu0112@proton.me>
2023-03-21 13:18:38 +0900

    9_0321화

---
+++ 강의/9_0321화/factorial.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	printf("select int n: ");
+	scanf("%d", &n);
+
+	int res = 1;
+
+	// while로 쓰는 경우
+	int i = 1;
+	while (i <= n)
+	{
+		res = res * i;
+		i++;
+	}
+
+	// for으로 쓰는 경우
+	// for (int i = 1; i <= n; i++)
+	// {
+	// 	res = res * i;
+	// }
+
+	printf("factorial %d is %d\n", n, res);
+	return 0;
+}
