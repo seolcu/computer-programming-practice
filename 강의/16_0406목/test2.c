14492ee330c122233601b7c178379780f8a193b8
seolcu <seolcu0112@proton.me>
2023-04-11 02:53:22 +0900

    fixed test2.c

--- 강의/16_0406목/test2.c
+++ 강의/16_0406목/test2.c
@@ -1,32 +1,57 @@
 // 설규원 202322071
 #include <stdio.h>
 
-int main() {
+int main()
+{
 	int size;
 	printf("How many element? (bigger than 10): ");
 	scanf("%d", &size);
 
 	int numList[size];
 
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < size; i++)
+	{
 		printf("Enter an int number %d: ", i);
 		scanf("%d", &numList[i]);
 	}
 
-	int midIndex = size / 2, leftSum, rightSum;
-	for (int i = 0; i < size; i++) {
-		if (i < midIndex) {
-			leftSum += numList[i];
-		} else if (i >= midIndex) {
-			rightSum += numList[i];
+	int midIndex = size / 2, leftSum = 0, rightSum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (size % 2 == 1)
+		{
+			if (i < midIndex)
+			{
+				leftSum += numList[i];
+			}
+			else if (i > midIndex)
+			{
+				rightSum += numList[i];
+			}
+		}
+		else if (size % 2 == 0)
+		{
+			if (i < midIndex)
+			{
+				leftSum += numList[i];
+			}
+			else if (i >= midIndex)
+			{
+				rightSum += numList[i];
+			}
 		}
 	}
 
-	if (leftSum > rightSum) {
+	if (leftSum > rightSum)
+	{
 		printf("The array is left-skewed\n");
-	} else if (leftSum < rightSum) {
+	}
+	else if (leftSum < rightSum)
+	{
 		printf("The array is right-skewed\n");
-	} else {
+	}
+	else
+	{
 		printf("The array is balanced\n");
 	}
 	return 0;
