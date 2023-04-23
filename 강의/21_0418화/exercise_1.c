86963529bb34a90fbfe2c2a351dcedb107290a74
seolcu <seolcu0112@proton.me>
2023-04-23 22:40:11 +0900

    README 수정, 코드 명료화

--- 강의/21_0418화/exercise_1.c
+++ 강의/21_0418화/exercise_1.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
-void getSumAndAverage(int a, int b, int c, int *sum, double *average);
+void getSumAndAverage(int num1, int num2, int num3, int *sumValue, double *averageValue);
 
 int main()
 {
-    int sum;
+    int a, b, c, sum;
     double average;
-    getSumAndAverage(1, 3, 5, &sum, &average);
-    printf("%d %lf\n", sum, average);
+
+    printf("Enter three ints: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    getSumAndAverage(a, b, c, &sum, &average);
+    printf("sum: %d\naverage: %lf\n", sum, average);
+
     return 0;
 }
 
-void getSumAndAverage(int a, int b, int c, int *sum, double *average)
+void getSumAndAverage(int num1, int num2, int num3, int *sumValue, double *averageValue)
 {
-    *sum = a + b + c;
-    *average = (a + b + c) / 3;
+    *sumValue = num1 + num2 + num3;
+    *averageValue = *sumValue / 3;
 }
